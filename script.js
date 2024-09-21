b52b909b807616b0e331c6821811db7a8cdf7773
farhan <ff1917914@gmail.com>
2024-09-21 12:30:32 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+
+
+
+
+
+
+
+// chapter no 13 /////////////// (if statement)
+
+
+
+
+// Q1////////
+
+// if (a === b && c === d) {
+
+// }
+
+
+// Q2///////////
+
+
+// if (a === b || c !== d) {
+
+// }
+
+
+// Q3
+
+
+// var name = "hamza"
+// var name = "arsalan"
+// var age = 60
+
+// if ((name === "Hamza" || name === "Arsalan") && age < 60) {
+
+// }
+
+// Q4
+
+
+// var num1 = 5; 
+// var num2 = 10;
+
+// if (num1 < num2 || num1 > num2) {
+//     alert("The first variable is either less than or greater than the second variable.");
+// }
+
+// Q5
+
+// var firstName = "YourFirstName"; 
+// var lastName = "YourLastName"; 
+
+// var userFirstName = prompt("Please enter your first name:");
+// var userLastName = prompt("Please enter your last name:");
+
+// if (userFirstName === firstName && userLastName === lastName) {
+//     alert("Welcome!");
+// }
+
+// Q6
+
+// chapter no 14
+
+// Q1
+
+// var password = "sir faraz"; 
+
+// if (password !== "sir faraz") {
+//     if (password.length <= 5) {
+//         alert("Password must be greater than 5");
+//     } else {
+//         alert("OK");
+//     }
+// }
+
+
+// Q2
+
+
+// var a = 1;
+// var c = "Max"; 
+
+// if (a === 1) {
+//     if (c === "Max") {
+//         alert("OK");
+//     }
+// }
+
+
+
+
+
